8ac18117f3a8eed507b405c5325e27b839239a24
Joan Andrés <andres6936@live.com>
2020-09-15 09:39:22 -0500

    Simplify the method index

--- pages/views.py
+++ pages/views.py
@@ -3,9 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request, pagename):
-    if pagename is '':
-        return render(request, 'view.html')
-    elif pagename is 'add':
-        return render(request, 'add.html')
-    elif pagename is 'view':
-        return render(request, 'view.html')
+    return render(request, pagename + '.html')
