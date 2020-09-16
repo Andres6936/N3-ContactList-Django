7c1ca56026f78329f8d88f798db6c064aed79506
Joan Andrés <andres6936@live.com>
2020-09-16 11:38:29 -0500

    Created page Home and redirect the url to Home

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, {'pagename': ''}, name='home'),
+    path('', views.index, {'pagename': 'home'}, name='home'),
     path('add', views.add, name='add'),
+    path('home', views.index, {'pagename': 'home'}, name='home'),
     path('view', views.index, {'pagename': 'view'}, name='view'),
 ]
